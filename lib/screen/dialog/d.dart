3b2330107ee35a089ba95292ec2515ce37059e6b
Bansook Nam <bs.nam@lawfully.com>
2023-09-13 19:54:39 +0900

    add live template for dialog

---
+++ lib/screen/dialog/d.dart
@@ -0,0 +1,29 @@
+// import 'package:fast_app_base/common/widget/scaffold/center_dialog_scaffold.dart';
+// import 'package:flutter/material.dart';
+// import 'package:nav/dialog/dialog.dart';
+//
+// import '../../common/common.dart';
+//
+//
+// class $NAME$Dialog extends DialogWidget<$RESULT_TYPE$> {
+//   $NAME$Dialog({super.key});
+//
+//   @override
+//   State<$NAME$Dialog> createState() => _$NAME$DialogState();
+// }
+//
+// class _$NAME$DialogState extends State<$NAME$Dialog> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return CenterDialogScaffold(
+//         body: Column(
+//           children: [
+//             Container(
+//               height: 300,
+//               width: 300,
+//               color: Colors.blue,
+//             )
+//           ],
+//         ));
+//   }
+// }
